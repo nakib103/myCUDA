2262c98fe6fd6ebad48f782f368bc1c3ac172b13
Syed Nakib Hossain <syedhossain103@gmail.com>
2021-08-09 07:04:51 +0600

    use absolute path for cuda program file

--- run_cuda.py
+++ run_cuda.py
@@ -1,5 +1,9 @@
 import subprocess
+import os
 
-cuda_program = "sample_kernel.cu"
-subprocess.run(["nvcc", cuda_program, "-o", "add_cuda"])
+current_dir = os.getcwd()
+cuda_program_file = "sample_kernel.cu"
+cuda_program_path = os.path.join(current_dir, cuda_program_file)
+
+subprocess.run(["nvcc", cuda_program_path, "-o", "add_cuda"])
 subprocess.run(["./add_cuda"], capture_output=True)
