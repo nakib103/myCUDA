2cba601fd66a53f2198b2a7ec67026aa3e21405d
Syed Nakib Hossain <syedhossain103@gmail.com>
2021-08-14 22:17:18 +0600

    initialize django project

---
+++ executor/views.py
@@ -0,0 +1,16 @@
+import subprocess
+import os
+
+from django.shortcuts import render
+
+def runCUDA(request):
+    current_dir = os.getcwd()
+    cuda_program_file = "sample_kernel.cu"
+    cuda_program_path = os.path.join(current_dir, cuda_program_file)
+
+    object_file = "add_cuda"
+    object_file_path = os.path.join(current_dir, object_file)
+
+    subprocess.run(["nvcc", cuda_program_path, "-o", object_file_path])
+    p = subprocess.run([object_file_path], stdout=subprocess.PIPE)
+    print(p.stdout)
